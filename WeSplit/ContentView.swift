2d0c8bbf5c8504c438e04631372c8ef61d65e8f4
Antonio J Rossi <antoniojrossi@gmail.com>
2020-02-01 17:37:22 +0100

    Add challenges

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,8 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = "2"
+    @State private var tipPercentage = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
+    var totalCheckAmount: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let numberFormatter = NumberFormatter()
+        numberFormatter.decimalSeparator = ","
+        let orderAmount = numberFormatter.number(from: checkAmount)?.doubleValue ?? 0
+        return orderAmount + (orderAmount / 100 * tipSelection)
+    }
+    var totalPerPerson: Double {
+        let peopleCount = Int(numberOfPeople) ?? 1
+        let amountPerPerson = totalCheckAmount / Double(peopleCount)
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+//                    Picker("Number of people", selection: $numberOfPeople) {
+//                        ForEach(2 ..< 100) {
+//                            Text("\($0) people")
+//                        }
+//                    }
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
+                }
+                Section(header: Text("How much tip do yo want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Total amount for the check")) {
+                    Text("\(totalCheckAmount, specifier: "%.2f") €")
+                }
+                Section(header: Text("Amount per person")) {
+                    Text("\(totalPerPerson, specifier: "%.2f") €")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
 
